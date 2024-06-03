33e98e4f8a996b737f9e98649a82988b074e583a
Yang Si Jun <aersijun@gmail.com>
2024-06-03 18:38:02 +0800

    longestconsecseq(HARD)

---
+++ ArraysAndHashing/longestConsecSeq.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        max_length = 0
+        for num in nums:
+            # if number is start of a seq
+            if (num - 1) not in num_set:
+                length = 1
+                while (num+length) in num_set:
+                    length += 1
+                max_length = max(max_length, length)
+        return max_length
